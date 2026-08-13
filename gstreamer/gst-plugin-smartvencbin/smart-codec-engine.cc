a846440d2f8f1ced5ad0884017ecd6308e585f6d
Bala Sai Kosuri <bkosuri@qti.qualcomm.com>
2026-08-13 09:47:23 +0530

    smartvencbin: Propagate name changes for video-ctrl
    - libVideoCtrl.so renamed as libqimsdk-smartvenc.so
    - Header install path updated
    
    Signed-off-by: Bala Sai Kosuri <bkosuri@qti.qualcomm.com>
    Signed-off-by: Girish K <girik@qti.qualcomm.com>

--- gstreamer/gst-plugin-smartvencbin/smart-codec-engine.cc
+++ gstreamer/gst-plugin-smartvencbin/smart-codec-engine.cc
@@ -9,7 +9,7 @@
 #include <dlfcn.h>
 #include <mutex>
 
-#include <iot-core-algs/videoctrl.h>
+#include <qimsdk-smartvenc/videoctrl.h>
 
 struct _SmartCodecEngine {
   std::mutex           engine_lock;
@@ -45,7 +45,7 @@ gst_smartcodec_engine_new ()
   SmartCodecEngine *engine = NULL;
   ::videoctrl::NewIEngine NewVideoCtrlEngine;
 
-  std::string libname = "libVideoCtrl.so." + std::string(VIDEO_CTRL_VERSION_MAJOR);
+  std::string libname = "libqimsdk-smartvenc.so." + std::string(VIDEO_CTRL_VERSION_MAJOR);
 
   engine = g_new0 (SmartCodecEngine, 1);
   g_return_val_if_fail (engine != NULL, NULL);
